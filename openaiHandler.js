40c981bdfc4cf7a2721955ac277bcbb5ac79db45
Gizdulock <krisz920803@gmail.com>
2023-06-16 14:59:52 +0200

    Create openaiHandler.js

---
+++ openaiHandler.js
@@ -0,0 +1,15 @@
+const { OpenAI } = require('openai');
+
+async function generateResponse(apiKey, prompt) {
+  const openai = new OpenAI(apiKey);
+  const gptResponse = await openai.complete({
+    engine: 'text-davinci-002',
+    prompt: prompt,
+    maxTokens: 60
+  });
+  return gptResponse.choices[0].text.trim();
+}
+
+module.exports = {
+  generateResponse
+};
