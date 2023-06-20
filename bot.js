ae7bf41828c3632d57546c58e1e0645c2791c049
pogoweb <pogoweb@ironscanner.hu>
2023-06-21 01:18:51 +0200

    push

--- bot.js
+++ bot.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
+const { prefix } = require('./config.json');
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
-const { handleCommand } = require('./commands/handleCommand.js');
+const { DISCORD_BOT_TOKEN } = process.env;
+const { handleCommand } = require('./handleCommand.js');
 
-const client = new Client({ intents: [Intents.FLAGS.Guilds, Intents.FLAGS.GuildMessages] });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING
+  ]
+});
 
 client.on('messageCreate', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -10,4 +22,12 @@ client.on('messageCreate', message => {
   handleCommand(message);
 });
 
-client.login(token);
+
+client.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  handleCommand(message);
+});
+
+client.login(DISCORD_BOT_TOKEN);
+
