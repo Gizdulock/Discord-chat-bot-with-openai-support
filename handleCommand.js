c7370d0f3ad07e2c708e7e8760034b9bdc03e7b6
Gizdulock <krisz920803@gmail.com>
2023-06-22 03:15:06 +0200

    Update handleCommand.js

--- handleCommand.js
+++ handleCommand.js
@@ -1,6 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { CommandInteraction } = require('discord.js');
 const quotaHandler = require('./quotaHandler');
+const openai = require('openai');
+
+openai.apiKey = process.env.API_KEY;
+
+function handleCommand(message, prefix) {
+  const commandName = message.content.slice(prefix.length).trim().split(/ +/)[0];
+
+  const command = commands.find(cmd => cmd.data.name === commandName);
+
+  if (!command) return;
+
+  command.execute(message);
+}
+
 
 const commands = [
   {
@@ -19,9 +33,28 @@ const commands = [
   {
     data: new SlashCommandBuilder().setName('help').setDescription('Display list of commands.'),
     async execute(interaction = new CommandInteraction()) {
-      await interaction.reply('Available commands: /quota, /status, /help');
+      await interaction.reply('Available commands: /quota, /status, /help, /chat');
+    }
+  },
+  {
+    data: new SlashCommandBuilder().setName('chat').setDescription('Start a conversation with ChatGPT.'),
+    async execute(interaction = new CommandInteraction()) {
+      const prompt = interaction.options.getString('message');
+      const response = await openai.Completion.create({
+        engine: 'text-davinci-002',
+        prompt: prompt,
+        max_tokens: 60
+      });
+
+      const messages = response.choices[0].text.trim().split(/\n|\r/);
+      for (const message of messages) {
+        await interaction.reply(message);
+        await new Promise(resolve => setTimeout(resolve, 200));
+      }
     }
   }
 ];
 
-module.exports = commands;
+module.exports = {
+  handleCommand,
+};
