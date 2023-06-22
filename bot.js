7dc3f0229fb0e4bf2f6f0a1d80a00ade65037ac5
Gizdulock <krisz920803@gmail.com>
2023-06-22 03:26:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const { prefix } = require('./config.json');
 const { Client, Intents } = require('discord.js');
-const { DISCORD_BOT_TOKEN } = process.env;
+const { DISCORD_BOT_TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 const { handleCommand } = require('./handleCommand.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
 const client = new Client({
   intents: [
@@ -19,8 +20,31 @@ const client = new Client({
 client.on('messageCreate', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  handleCommand(message, prefix);
+  handleCommand(message);
 });
 
+client.on('messageCreate', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  handleCommand(message);
+});
+
+const commands = []; // A parancsokat tartalmazó tömb
+const rest = new REST({ version: '9' }).setToken(DISCORD_BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 client.login(DISCORD_BOT_TOKEN);
