b148f47ebb003487560b812616f77f8cd71ce2b9
Gizdulock <krisz920803@gmail.com>
2023-06-23 08:16:04 +0200

    Update handleCommand.js

--- handleCommand.js
+++ handleCommand.js
@@ -1,62 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { CommandInteraction } = require('discord.js');
-const quotaHandler = require('./quotaHandler');
 const openai = require('openai');
-const { generateResponse } = require('./openaiHandler');
-
+require('dotenv').config();
 
 openai.apiKey = process.env.OPENAI_API_KEY;
 
-function handleCommand(interaction) {
-  const commandName = interaction.commandName;
+async function execute(interaction) {
+  const message = interaction.options.getString('message');
+  const prompt = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nUser: ${message}\nAI:`;
 
-  const command = commands.find(cmd => cmd.data.name === commandName);
-
-  if (!command) return;
-
-  command.execute(interaction);
-}
-
-
-const commands = [
-  {
-    data: new SlashCommandBuilder().setName('quota').setDescription('Display current API quota usage.'),
-    async execute(interaction = new CommandInteraction()) {
-      const quota = await quotaHandler.getQuota();
-      await interaction.reply(`Current API quota usage: ${quota}`);
-    }
-  },
-  {
-    data: new SlashCommandBuilder().setName('status').setDescription('Display bot status.'),
-    async execute(interaction = new CommandInteraction()) {
-      await interaction.reply('Bot is online.');
-    }
-  },
-  {
-    data: new SlashCommandBuilder().setName('help').setDescription('Display list of commands.'),
-    async execute(interaction = new CommandInteraction()) {
-      await interaction.reply('Available commands: /quota, /status, /help, /chat');
-    }
-  },
-  {
-    data: new SlashCommandBuilder().setName('chat').setDescription('Start a conversation with ChatGPT.'),
-    async execute(interaction = new CommandInteraction()) {
-      const prompt = interaction.options.getString('message');
-      const response = await openai.Completion.create({
-        engine: 'text-davinci-002',
-        prompt: prompt,
-        max_tokens: 60
-      });
-
-      const messages = response.choices[0].text.trim().split(/\n|\r/);
-      for (const message of messages) {
-        await interaction.reply(message);
-        await new Promise(resolve => setTimeout(resolve, 200));
-      }
-    }
+  try {
+    console.log('Sending prompt to OpenAI API:', prompt);
+    const response = await openai.Completion.create({
+      engine: 'text-davinci-002',
+      prompt: prompt,
+      max_tokens: 60
+    });
+    console.log('Received response from OpenAI API:', response);
+    await interaction.reply(response.choices[0].text.trim());
+  } catch (error) {
+    console.error('Error occurred during OpenAI API call:', error);
+    await interaction.reply('An error occurred while processing your request. Please try again later.');
   }
-];
+};
 
 module.exports = {
-  handleCommand,
+  execute,
 };
