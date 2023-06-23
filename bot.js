44df7dd0e837e314d9e7444c63379dc37f3bbfc7
Gizdulock <krisz920803@gmail.com>
2023-06-23 02:15:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ const { Routes } = require('discord-api-types/v9');
 const { guildId, prefix } = require('./config.json');
 const { loadConfig } = require('./configHandler.js');
 const openai = require('openai');
-const { OpenAI } = require('openai-api');
 
 const config = loadConfig();
 openai.apiKey = process.env.OPENAI_API_KEY;
