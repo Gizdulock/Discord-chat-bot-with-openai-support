b2dead694f000bd96ea7c401c706020cc8724b7c
Gizdulock <krisz920803@gmail.com>
2023-06-23 14:05:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,33 @@
-require('dotenv').config();
-const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents } = require('discord.js');
-const { handleCommand } = require('./handleCommand.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 const client = new Client({
   intents: [
@@ -16,23 +42,15 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
-client.once('ready', async () => {
+client.once('ready', () => {
   console.log('Ready!');
-
-  // Register all commands as global commands
-  const commands = client.commands.map(({ data }) => data);
-  await client.application.commands.set(commands);
-
-  console.log('Commands registered!');
 });
- 
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
@@ -45,8 +63,4 @@ client.on('interactionCreate', async interaction => {
     await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
-
-client.login(process.env.DISCORD_BOT_TOKEN);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true
