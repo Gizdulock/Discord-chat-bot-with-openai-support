9e10b0efa7c22a9a7cc3b671d9a4a86f8484660f
Gizdulock <krisz920803@gmail.com>
2023-06-23 14:26:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
+const config = require('./config.json');
 require('dotenv').config();
 
 const commands = [];
@@ -19,10 +20,9 @@ const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, config.guildId),
       { body: commands },
     );
-
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
