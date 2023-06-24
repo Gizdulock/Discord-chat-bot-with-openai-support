a1b43099017e3ff5c2911e54ae796b5bf0714ed3
Gizdulock <krisz920803@gmail.com>
2023-06-24 02:06:40 +0200

    Update openaiHandler.js

--- openaiHandler.js
+++ openaiHandler.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
-const { OpenAI } = require('openai');
+const OpenAI = require('openai');
 
-async function generateResponse(apiKey, prompt) {
-  const openai = new OpenAI(apiKey);
-  const gptResponse = await openai.complete({
-    engine: 'text-davinci-002',
-    prompt: prompt,
-    maxTokens: 60
-  });
-  return gptResponse.choices[0].text.trim();
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+async function generateResponse(message) {
+  const prompt = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nUser: ${message}\nAI:`;
+
+  try {
+    const gptResponse = await openai.complete({
+      engine: 'text-davinci-003',
+      prompt: prompt,
+      maxTokens: 100,
+      temperature: 0.7,
+      topP: 1,
+      frequencyPenalty: 0,
+      presencePenalty: 0,
+    });
+
+    const response = gptResponse.data.choices[0].text.trim();
+    return response;
+  } catch (error) {
+    console.error('Error occurred during OpenAI API call:', error);
+    throw error;
+  }
 }
 
 module.exports = {
