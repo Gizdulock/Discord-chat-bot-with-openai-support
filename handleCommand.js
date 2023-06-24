102720e067045fa71b111fb9eb5b6deaa33ba947
Gizdulock <krisz920803@gmail.com>
2023-06-24 02:05:36 +0200

    Update handleCommand.js

--- handleCommand.js
+++ handleCommand.js
@@ -1,35 +1,17 @@
 require('dotenv').config();
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { OpenAIApi } = require('openai');
-const openai = new OpenAIApi(process.env.OPENAI_API_KEY);
+const { generateResponse } = require('./openaiHandler');
 
 async function execute(interaction) {
   const message = interaction.options.getString('message');
-  const prompt = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nUser: ${message}\nAI:`;
+  const response = await generateResponse(message);
 
   try {
-    console.log('Sending prompt to OpenAI API:', prompt);
-    async function generateResponse(prompt) {
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.9,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      return response.choices[0].text.trim();
-    }
-
-    const response = await generateResponse(prompt); // Add this line
-    console.log('Received response from OpenAI API:', response);
     await interaction.reply(response);
   } catch (error) {
-    console.error('Error occurred during OpenAI API call:', error);
-    await interaction.reply('An error occurred while processing your request. Please try again later.');
+    console.error('Error occurred during interaction reply:', error);
   }
-};
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
